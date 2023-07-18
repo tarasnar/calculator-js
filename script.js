c47ef43aad1ea031344e3610c8a3ad617dda4425
Taras <andergraund420@gmail.com>
2023-07-18 19:31:48 +0300

    All of the buttons added, functions for calculating different operators added, logic for basic evaluation added

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-//Make the buttons output desired numbers
-const numbersButtons = document.querySelectorAll('.numbers');
+//Create function for addition, subtraction, multiplication and division
+function addition(num1, num2) {
+    console.log(num1 + num2);
+}
 
-let number = [];
-numbersButtons.forEach(button => {
+function subtraction(num1, num2) {
+    console.log(num1 - num2);
+}
+
+function multiplication(num1, num2) {
+    console.log(num1 * num2);
+}
+
+function division(num1, num2) {
+    console.log(num1 / num2);
+}
+//Create variable for first number, operator and second number
+let num1;
+let operator;
+let num2;
+const numberButtons =
+    document.querySelectorAll('.numbers').forEach(button => {
     button.addEventListener('click', () => {
-        number.push(+button.innerText);
+        num1 = +button.innerText;
     });
 });
-//Create functions for calculating addition, substraction, multiplication, division
+const operatorButtons =
+    document.querySelectorAll('.operators').forEach(button => {
+    button.addEventListener('click', () => operator = button.innerText);
+});
+//Create function operate which will perform operations on these three variables
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            console.log(addition(num1, num2));
+            break;
+
+        case '-':
+            console.log(subtraction(num1, num2));
+            break;
+
+        case '*':
+            console.log(multiplication(num1, num2));
+            break;
+
+        case '/':
+            console.log(division(num1, num2));
+            break;
+    }
+}
+const evaluationButton = document.querySelector
+('.evaluation').addEventListener('click',
+    () => operate(num1, operator, num2));
