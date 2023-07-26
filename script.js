29433d6f637aa429f5d2956b1b2e4c23c90da7bc
Taras <andergraund420@gmail.com>
2023-07-26 19:41:03 +0300

    calculator is now fully functional, looks better than ever and only lacks rounding

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const screen = document.querySelector('#calc-screen');
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operators');
+const evaluationButton = document.querySelector('.evaluation');
+const clearButton = document.querySelector('.clear');
+const percentButton = document.querySelector('#percent');
+
+//Create variables to store first number, operator, second number
+let num1 = [];
+let operator;
+let num2 = [];
+
 //Create functions for addition, subtraction, multiplication and division
 function addition(num1, num2) {
     return num1 + num2;
@@ -12,97 +24,139 @@ function multiplication(num1, num2) {
 }
 
 function division(num1, num2) {
-    return num1 / num2;
+    if (num2 !== 0) {
+        return num1 / num2;
+    } else {
+        clear();
+        return 'hell no';
+    }
 }
 
-let screen = document.querySelector('#calc-screen');
-//Create variable to store first number, operator, second number
-let num1 = ['0'];
-screen.innerText = num1.join('');
-let operator;
-let num2 = [];
+//Function that decides to which element to append to
+function decideInput(element, button) {
+    if (button.innerText === '+/-') {
+        if (findPlusMinus(element) !== '-') {
+            element.unshift('-');
+            updateScreen(element);
+        } else {
+            element.shift();
+            updateScreen(element);
+        }
+    } else if (button.innerText !== '0' && element[0] === '0') {
+        if (button.innerText === '.') {
+            if (findDot(element) !== '.') {
+                element.push(button.innerText);
+                updateScreen(element);
+            }
+        } else if (findDot(element) !== '.') {
+            element.splice(0);
+            element.push(button.innerText);
+            updateScreen(element);
+        } else {
+            element.push(button.innerText);
+            updateScreen(element);
+        }
+    } else if (button.innerText === '0') {
+        if (element[0] !== '0' || findDot(element) === '.') {
+            element.push(button.innerText);
+            updateScreen(element);
+        }
+    } else {
+        element.push(button.innerText);
+        updateScreen(element);
+    }
+}
 
-//Start event listening on all number buttons
-const numberButtons = document.querySelectorAll('.numbers');
+//Function that performs operations based on the operator
+function operate(num1, operator, num2) {
+    num1 = Number(num1.join(''));
+    num2 = Number(num2.join(''));
+    switch(operator) {
+        case '+':
+            return String(addition(num1, num2));
+
+        case '-':
+            return String(subtraction(num1, num2));
+
+        case '*':
+            return String(multiplication(num1, num2));
+
+        case '/':
+            return String(division(num1, num2));
+    }
+}
+
+function findPlusMinus(element) {
+    return element.find(element => element === '-');
+}
+
+//Function which checks element for a dot
+function findDot(element) {
+    return element.find(element => element === '.');
+}
+
+//Function to update screen
+function updateScreen(element) {
+    screen.innerText = element.join('');
+}
+
+//Function to empty variables and start over
+function clear() {
+    num1 = [];
+    num2 = [];
+    operator = undefined;
+}
+
+function evaluate() {
+    num1 = [operate(num1, operator, num2)];
+    updateScreen(num1);
+    num2 = [];
+}
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if (operator === undefined) {
-            //If we are in num1 and press 0
-            if (button.innerText !== '0' && num1[0] === '0') {
-                num1 = [];
-                num1.push(button.innerText);
-                screen.innerText = num1.join('');
-            } else if (button.innerText === '0' && num1[0] !== '0') {
-                num1.push(button.innerText);
-                screen.innerText = num1.join('');
-            }
+        if (operator === undefined || num1.length < 1) {
+            decideInput(num1, button);
         } else {
-            num2.push(button.innerText);
-            screen.innerText = num2.join('');
+            decideInput(num2, button);
         }
     });
 });
 
-//Start event listening on all operator buttons
-const operatorButtons = document.querySelectorAll('.operators');
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
         if (operator !== undefined && num1.length > 0 && num2.length > 0) {
-                num1[0] = operate(num1, operator, num2);
-                screen.innerText = num1[0];
-                num1.splice(1);
-                num2 = [];
-                operator = button.innerText;
+            evaluate();
+            operator = button.innerText;
         } else {
             operator = button.innerText;
         }
     });
 });
 
-//Create function operate which will perform operations based on the operator
-function operate(num1, operator, num2) {
-    num1 = Number(num1.join(''));
-    num2 = Number(num2.join(''));
-    switch(operator) {
-        case '+':
-            return addition(num1, num2);
-
-        case '-':
-            return subtraction(num1, num2);
-
-        case '*':
-            return multiplication(num1, num2);
-
-        case '/':
-            if (num2 === 0) {
-                return 'hell no';
-            } else {
-                return division(num1, num2);
-            }
+percentButton.addEventListener('click', () => {
+    let percentage;
+    if (screen.innerText === num1.join('')) {
+        percentage = Number(num1.join('') / 100);
+        num1.splice(0);
+        num1.push(percentage);
+        updateScreen(num1);
+    } else if (screen.innerText === num2.join('')) {
+        percentage = Number(num2.join('') / 100);
+        num2.splice(0);
+        num2.push(percentage);
+        updateScreen(num2);
     }
-}
+})
 
-//Start event listening on evaluation button
-const evaluationButton = document.querySelector('.evaluation');
 evaluationButton.addEventListener('click', () => {
-    screen.innerText = operate(num1, operator, num2);
+    evaluate();
     clear();
 });
 
-//Function to empty variables and start over
-function clear() {
-    num1 = ['0'];
-    num2 = [];
-    operator = undefined;
-}
-
-//Button calling the clear function and displaying the initial value
-const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
     clear();
-    screen.innerText = num1.join('');
+    updateScreen(num1);
 });
 
-//Make . button work and append to array 1 or 2 once
-//Make +/- work and put the correct sign at the start of a number
-//Make % button work and output 1% of the number present
+//Make dem round
